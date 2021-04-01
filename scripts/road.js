86a4919d6ae029f64dca928f1fa40beb3a574f4b
Song-Qian <onlylove1172559463@vip.qq.com>
2021-04-01 14:51:20 +0800

    ignore

--- scripts/road.js
+++ scripts/road.js
@@ -7,7 +7,7 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
     typeof define === 'function' && define.amd ? define(['exports'], factory) :
-    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.intersection = {}));
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.road = {}));
 }(this, (function (exports) { 
     'use strict';
 
